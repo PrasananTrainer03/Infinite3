77aa99475ea5249c36a824551386f01f28b6c4a6
Prasanna <prassup.dotnet@gmail.com>
2021-12-09 11:02:43 +0530

    code

---
+++ Day1_Part_1/DemoProject/src/com/java/demo/Data.java
@@ -0,0 +1,23 @@
+package com.java.demo;
+
+class Demo {
+	
+	public void basin() {
+		System.out.println("Hi I am Basin Kumar...");
+	}
+	
+	void dinesh() {
+		System.out.println("Hi I am Dinesh...");
+	}
+	
+	private void shahida() {
+		System.out.println("Hi I am Shahida...");
+	}
+}
+public class Data {
+	public static void main(String[] args) {
+		Demo obj = new Demo();
+		obj.basin();
+		obj.dinesh();
+	}
+}

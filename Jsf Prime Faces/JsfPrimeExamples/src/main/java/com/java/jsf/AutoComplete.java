c5abd9a38be5e017694a7aafa679bee063256a59
Prasanna <prassup.dotnet@gmail.com>
2022-01-31 17:14:41 +0530

    code

---
+++ Jsf Prime Faces/JsfPrimeExamples/src/main/java/com/java/jsf/AutoComplete.java
@@ -0,0 +1,30 @@
+package com.java.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="autoComplete")
+public class AutoComplete {
+
+	private String studentName;
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+	public List<String> students() {
+		List<String> studentList = new ArrayList<String>();
+		studentList.add("Aishwarya");
+		studentList.add("Krishna");
+		studentList.add("Manish");
+		studentList.add("Murali");
+		studentList.add("Ibrahim");
+		return studentList;
+	}
+}
